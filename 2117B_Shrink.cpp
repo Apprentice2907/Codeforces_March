feb600b89805cd64cc7c4f1c66c031742d5375e7
PRINCE BHADANIA <princebhadania007@gmail.com>
2026-03-28 19:12:33 +0530

    Day-28

---
+++ 2117B_Shrink.cpp
@@ -0,0 +1,22 @@
+// My approach
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        for (int i = 2; i <= n; i++) {
+            cout << i << " ";
+        }
+
+        cout << 1 << endl;
+    }
+
+    return 0;
+}
---
+++ 2118A_Equal_Subsequences.cpp
@@ -0,0 +1,26 @@
+// My approach 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+
+        for (int i = 0; i < k; i++) {
+            cout << '1';
+        }
+
+        for (int i = 0; i < n - k; i++) {
+            cout << '0';
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
