f0a6f8a61c3aa5023102e002562f0c48aea0c2f2
PRINCE BHADANIA <princebhadania007@gmail.com>
2026-03-29 22:25:50 +0530

    Day-29

---
+++ 2116A_Gellyfish_Tricolor_Pansy.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        long long a, b, c, d;
+        cin >> a >> b >> c >> d;
+
+        if (min(a, c) >= min(b, d))
+            cout << "Gellyfish\n";
+        else
+            cout << "Flower\n";
+    }
+
+    return 0;
+}
---
+++ 2117A_False_Alarm.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+
+        int first = -1, last = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (a[i] == 1) {
+                if (first == -1) first = i;
+                last = i;
+            }
+        }
+
+        int length = last - first + 1;
+
+        if (length <= x) cout << "YES\n";
+        else cout << "NO\n";
+    }
+
+    return 0;
+}
